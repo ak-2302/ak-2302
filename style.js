9b484134bc817e5649d615933cec74b4d394ea85
ak-2302 <s2211879@s.tsukuba.ac.jp>
2025-04-14 03:50:23 +0900

    why

--- style.js
+++ style.js
@@ -11,24 +11,49 @@ content_qiita = document.getElementById("content_qiita");
 content_twitter.style.display = "none";
 content_github.style.display = "none";
 content_qiita.style.display = "none";
-
+let open_tab = null;
 document.getElementById("tab_twitter").addEventListener("click", function () {
-    content_twitter.style.display = "block";
-    content_github.style.display = "none";
-    content_qiita.style.display = "none";
+    if (open_tab != "tab_twitter") {
+        open_tab = "tab_twitter";
+        content_twitter.style.display = "block";
+        content_github.style.display = "none";
+        content_qiita.style.display = "none";
+    } else {
+        open_tab = null;
+        content_twitter.style.display = "none";
+        content_github.style.display = "none";
+        content_qiita.style.display = "none";
+    }
 });
 document.getElementById("tab_github").addEventListener("click", function () {
-    content_twitter.style.display = "none";
-    content_github.style.display = "flex";
-    content_qiita.style.display = "none";
+    if (open_tab != "tab_github") {
+        open_tab = "tab_github";
+        content_twitter.style.display = "none";
+        content_github.style.display = "flex";
+        content_qiita.style.display = "none";
+    } else {
+        open_tab = null;
+        content_twitter.style.display = "none";
+        content_github.style.display = "none";
+        content_qiita.style.display = "none";
+    }
 });
 document.getElementById("tab_qiita").addEventListener("click", function () {
-    content_twitter.style.display = "none";
-    content_github.style.display = "none";
-    content_qiita.style.display = "block";
+    if (open_tab != "tab_qiita") {
+        open_tab = "tab_qiita";
+        content_twitter.style.display = "none";
+        content_github.style.display = "none";
+        content_qiita.style.display = "block";
+    } else {
+        open_tab = null;
+        content_twitter.style.display = "none";
+        content_github.style.display = "none";
+        content_qiita.style.display = "none";
+    }
 });
 
-window.addEventListener("scroll", () => {
+
+function progress_cat() {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrollPercent = (scrollTop / scrollHeight) * 100;
@@ -41,4 +66,15 @@ window.addEventListener("scroll", () => {
     cat.style.left = scrollPercent * (window.innerWidth - cat_width - cat2_width) / 100 + "px";
     cat.style.bottom = Math.max(0, Math.sin(scrollPercent / 100 * Math.PI * 5)) * cat_height / 4 + "px";
     cat2.style.bottom = Math.max(0, Math.sin(scrollPercent / 100 * Math.PI * 6)) * cat2_height / 4 + "px";
+}
+
+
+window.addEventListener("scroll", () => {
+    progress_cat();
 });
+window.addEventListener("resize", () => {
+    progress_cat();
+})
+window.addEventListener("click", () => {
+    progress_cat();
+})
