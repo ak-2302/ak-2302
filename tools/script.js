b64c62d2eb63c8ae66ab738a9c9383b00b7db7c2
ak-2302 <s2211879@s.tsukuba.ac.jp>
2025-04-18 01:17:16 +0900

    fix

--- tools/script.js
+++ tools/script.js
@@ -1,8 +1,12 @@
 const cards = document.querySelectorAll('.tool-wraper');
 
 cards.forEach(card => {
+    var src = card.querySelector("iframe").src;
+    if (src.endsWith("index.html")) {
+        src = src.replace(/index\.html$/, '');
+    }
+    card.style.cursor = "pointer";
     card.addEventListener('click', () => {
-        var src = card.querySelector("iframe").src;
         window.open(src, '_blank');
     });
 });
